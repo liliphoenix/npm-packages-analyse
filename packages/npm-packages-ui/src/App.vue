ba5655a2d8b30e2c3ac4cec3af8d568432237ba6
li-hao-dong <1990608687@qq.com>
2023-08-23 18:25:24 +0800

    移除前端中不必要的文件

--- packages/npm-packages-ui/src/App.vue
+++ packages/npm-packages-ui/src/App.vue
@@ -1,45 +1,12 @@
 <template>
-<!--    <div style="display: block">{{npmAnalyzeRes}}</div>-->
 <!-- echarts -->
   <treemapDrillDown :packageData="npmAnalyzeRes"/>
-<!--&lt;!&ndash; 方块方式 &ndash;&gt;-->
-<!--    <div v-if="npmAnalyzeRes && String(npmAnalyzeRes) !== '{}'">-->
-<!--      <drawRect-->
-<!--              data-floor="1"-->
-<!--              :width="width"-->
-<!--              :height="100"-->
-<!--              :detailData="dealDetailDate(svgColor['floor_'+1].bgColor, svgColor['floor_'+1].borderColor,-->
-<!--              <string>npmAnalyzeRes!.name, <string>npmAnalyzeRes!.version)" />-->
-<!--      <div class="child-box">-->
-<!--          <div v-for="(msg, key) in npmAnalyzeRes!.dependencies" :key="key"-->
-<!--               :data-test="msg!.dependencies.length"-->
-<!--               :class="msg!.dependencies.length==0? 'setBlock':'setInline'">-->
-<!--              <div style="display: inline-block">-->
-<!--                <drawRect-->
-<!--                        data-floor="2"-->
-<!--                        :width="0"-->
-<!--                        :height="100"-->
-<!--                        :detailData="dealDetailDate(svgColor['floor_'+2].bgColor, svgColor['floor_'+2].borderColor,-->
-<!--                        msg!.name, msg!.version)" />-->
-<!--              </div>-->
-<!--              <div v-if="msg!.dependencies.length>0" style="display: inline-block">-->
-<!--                &lt;!&ndash;  属性floor 由于组件中floor今日便+1，所以这里就给2了 &ndash;&gt;-->
-<!--                <renderMore :packageDate="msg!.dependencies" :colorObj="svgColor['floor_'+3]" :floor="3"/>-->
-<!--              </div>-->
-<!--          </div>-->
-<!--      </div>-->
-<!--    </div>-->
-
-<!--  <RouterView />-->
 </template>
 
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import {computed, onBeforeMount, onMounted, reactive, ref} from "vue";
-import DrawRect from "@/components/drawRect.vue";
 import {dealDetailDate} from "@/utils/formatDate";
-import RenderMore from "@/components/renderMore.vue";
 import {svgColor} from "@/config/colorMsg";
 import TreemapDrillDown from "@/components/treemapDrillDown.vue";
 
--- packages/npm-packages-ui/src/main.ts
+++ packages/npm-packages-ui/src/main.ts
@@ -5,11 +5,11 @@ import { createPinia } from 'pinia'
 
 // @ts-ignore
 import App from "./App.vue"
-import router from './router'
+// import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+// app.use(router)
 
 app.mount('#app')
